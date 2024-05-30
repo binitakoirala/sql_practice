9ee5ed667e2bb4201df2420b855fac0f4c6d1bb6
Binita Koirala <binitakoirala10@gmail.com>
2024-05-30 10:43:12 +0100

    Format code.

--- easy/solution_18.sql
+++ easy/solution_18.sql
@@ -2,10 +2,8 @@
 -- Find all athletes who participated in both 1992 and 1994.
 SELECT 
     DISTINCT name FROM public.olympics_history
-WHERE 
-    year = 1992 
+WHERE year = 1992 
 INTERSECT
 SELECT 
     DISTINCT name FROM public.olympics_history
-WHERE 
-    year = 1994;
+WHERE year = 1994;
