54eefa969b375e8a8bb84a773b2af973fcfa8fd1
Binita Koirala <binitakoirala10@gmail.com>
2024-05-30 16:12:11 +0100

    Add solution for challenge 7.

---
+++ intermediate/solution_7.sql
@@ -0,0 +1,11 @@
+-- 
+-- 
+WITH ranked_olympics AS (
+  SELECT DISTINCT name, weight,
+         RANK() OVER (ORDER BY CAST(weight AS DECIMAL) DESC) AS rank
+  FROM public.olympics_history
+  WHERE weight IS NOT NULL AND weight != 'NA'
+)
+SELECT name, weight
+FROM ranked_olympics
+WHERE rank = 1;
