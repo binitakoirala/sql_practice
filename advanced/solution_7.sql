c6f0da1d5a7fdc170ca3577824d1260b2f7cb293
binitakoirala <35364442+binitakoirala@users.noreply.github.com>
2024-06-04 22:29:59 +0100

    Fix logic.

--- advanced/solution_7.sql
+++ advanced/solution_7.sql
@@ -1,9 +1,7 @@
 -- 
 -- Find the average height and weight of athletes who won a medal.
 SELECT
-	medal,
-	AVG(CAST(height AS FLOAT)) AS avg_height,
-	AVG(CAST(weight AS FLOAT)) AS avg_weight
+    AVG(CAST(height AS FLOAT)) AS avg_height,
+    AVG(CAST(weight AS FLOAT)) AS avg_weight
 FROM public.olympics_history
-WHERE medal != 'NA' AND height != 'NA' AND weight != 'NA'
-GROUP BY medal;
+WHERE medal != 'NA' AND height != 'NA' AND weight != 'NA';
