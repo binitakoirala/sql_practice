9288741bd96be46d7843f18db29dd9a3401eb91a
binitakoirala <35364442+binitakoirala@users.noreply.github.com>
2024-06-05 22:53:56 +0100

    Format code.

--- advanced/solution_9.sql
+++ advanced/solution_9.sql
@@ -1,15 +1,15 @@
 -- 
 -- Find the most common age among athletes.
-WITH common_age AS(
-	SELECT
-		age,
-		COUNT(age) AS age_count,
-		RANK()OVER(ORDER BY COUNT(age) DESC) AS rank
-	FROM public.olympics_history
-	GROUP BY age
+WITH common_age AS (
+    SELECT
+        age,
+        COUNT(age) AS age_count,
+        RANK() OVER (ORDER BY COUNT(age) DESC) AS rank
+    FROM public.olympics_history
+    GROUP BY age
 )
 SELECT
-	age,
-	age_count
+    age,
+    age_count
 FROM common_age
 WHERE rank = 1;
