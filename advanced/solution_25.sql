3070d66fad80217e4b2a21d5f36467160a2a7142
binitakoirala <35364442+binitakoirala@users.noreply.github.com>
2024-06-06 17:39:41 +0100

    Capitalize keyword.

--- advanced/solution_25.sql
+++ advanced/solution_25.sql
@@ -1,6 +1,6 @@
 -- 
 -- Calculate the total number of events in which athletes from Denmark participated.
 SELECT
-    COUNT(event) as total_event_count
+    COUNT(event) AS total_event_count
 FROM public.olympics_history
 WHERE team = 'Denmark';
